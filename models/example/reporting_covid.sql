b65fe35659b94bd2ed0aba2dffb476fd24768651
Maryam Samani <maryam.samani@iemfg.com>
2025-05-08 19:43:41 +0000

    add schema data

--- models/example/reporting_covid.sql
+++ models/example/reporting_covid.sql
@@ -10,7 +10,7 @@ SELECT
     SUM(v.total_dose) AS total_vaccinated, 
     SUM(v.total_alloc_moderna) AS total_moderna, 
     SUM(v.total_alloc_pfizer) AS total_pfizer
-FROM {{ ref('us_state_covid') }} c
+FROM {{ ref('Us_State_Covid') }} c
 JOIN {{ ref('us_state_vaccinate') }} v 
     ON c.st = v.st
 GROUP BY c.state, c.st
