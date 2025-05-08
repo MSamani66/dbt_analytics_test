f8a074efaf42626e5d68f1387564235dd79c687b
Maryam Samani <maryam.samani@iemfg.com>
2025-05-08 19:24:35 +0000

    change reporting

--- models/example/reporting_covid.sql
+++ models/example/reporting_covid.sql
@@ -1,6 +1,22 @@
+{{ config(materialized='table') }}
 
--- Use the `ref` function to select from other models
+SELECT 
+    c.state,
+    c.st, 
+    SUM(c.positive) AS positive, 
+    SUM(c.negative) AS negative,
+    SUM(c.hospitalized) AS hospitalized, 
+    SUM(c.death) AS death, 
+    SUM(v.total_dose) AS total_vaccinated, 
+    SUM(v.total_alloc_moderna) AS total_moderna, 
+    SUM(v.total_alloc_pfizer) AS total_pfizer
+FROM {{ ref('us_state_covid') }} c
+JOIN {{ ref('us_state_vaccinate') }} v 
+    ON c.st = v.st
+GROUP BY c.state, c.st
 
-select *
-from {{ ref('Us_State_Covid') }}
+
+
+
+
 
